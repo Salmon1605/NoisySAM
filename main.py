fb33eae7bddafa65ac2feb8ef8b0460b1297cbd7
Nguyen Hung <hoanghung.10062005gl@gmail.com>
2026-04-05 20:42:24 +0700

    update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import pickle as pkl
 
 from utils.modelLoader import SAM1, MobileSAM, FastSAMModel 
-from utils.dataLoader import COCOLoader, VOCPascalLoader, ADE20KLoader 
+from utils.dataLoader import COCOLoader, VOCPascalLoader, ADE20KLoader, BSDS500Loader
 from utils.metrics import Metrics 
 from utils.noise_injection import NoiseInjection
 from utils.run import Experiment
@@ -26,7 +26,9 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-OUTPUT_DIR = r'C:\Users\ADMIN\Documents\SAM\Research\result'
+OUTPUT_DIR = r'C:\Users\ADMIN\Documents\SAM\Research\result' 
+THRESHOLD = 0.15
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # ===== DATA INITIALIZATION =======
  
@@ -40,6 +42,14 @@ voc_dataset = VOCPascalLoader(split='val')
 
 # ADE20K dataset 
 ade_dataset = ADE20KLoader(split='validation') 
+
+# BSDS500 dataset
+root = r'C:\Users\ADMIN\Documents\SAM\Research\src\Data\BSR_bsds500\BSR\BSDS500\data' 
+images_subdir = r'C:\Users\ADMIN\Documents\SAM\Research\src\Data\BSR_bsds500\BSR\BSDS500\data\images' 
+annoations_subdir = r'C:\Users\ADMIN\Documents\SAM\Research\src\Data\BSR_bsds500\BSR\BSDS500\data\groundTruth' 
+split = 'val' 
+
+bsds500_dataset = BSDS500Loader(image_subdir=images_subdir, annotations_subdir=annoations_subdir, root=root, split=split) 
 
 
 # ====== MODEL INITIALIZATION ======= 
@@ -89,131 +99,71 @@ noise = NoiseInjection()
 
 noise_dict = {
     "gaussian": noise._inject_gaussian_noise,
-    "poisson": noise._inject_poisson_noise,
-    "salt_pepper": noise._inject_salt_and_pepper_noise,
-    "speckle": noise._inject_speckle_noise,
-    "pixelate": noise._inject_pixelate,
-    "defocus": noise._inject_defocus_noise, 
-    "motion_blur": noise._motion_blur
+    "motion_blur": noise._motion_blur,
+    "snow": noise._inject_snow, 
+    "brightness": noise._inject_brightness, 
+    "contrast": noise._inject_contrast, 
+    "jpeg": noise._inject_JPEG
 }
+
+severities = [1, 3, 5]
 
 # ====== METRICS ====== 
 metrics = Metrics() 
 
 
-# ====== COCO EVALUATION ======= 
-# 1 - SAM_b 
-COCO_vit_b_configs = {
-    "dataset_name": "COCO",
-    "model_name": "SAM_b",
-    "sam_checkpoint": "C:\Users\ADMIN\Documents\SAM\Research\model\sam_vit_b_01ec64.pth",
-    "severities": [3],
-    "corruption_types":list(noise_dict.keys()),
-    "prompt_type": "bbox",
-    "metrics": ["dice", "iou"],
-    "primary_metric": "iou",
-    "device": "cuda",
-    "seed": 42,
-    "output_dir": OUTPUT_DIR,
-    "experiment_tag": "SAM_vit_b_coco"
-}
-coco_vit_b_experiment = Experiment(
-    model=sam_b_model, 
-    dataset=coco_dataset, 
-    noise_dict=noise_dict, 
-    severity=3
-)
-coco_vit_b_result = coco_vit_b_experiment._evaluate() 
-coco_vit_b_experiment._save_json(configurations=COCO_vit_b_configs)
 
+# ====== COCO EVALUATION ======
+coco_models = [ 
+    (sam_b_model, "SAM_b", sam_vit_b_checkpoint),
+    (sam_h_model, "SAM_h", sam_vit_h_checkpoint),
+    (mobileSAM_model, "MobileSAM", mobileSAM_checkpoint),
+    (fastSAM_model, "FastSAM", FastSAM_model)
+]
 
-# 2 - SAM_h 
-COCO_vit_h_configs = {
-    "dataset_name": "COCO",
-    "model_name": "SAM_h",
-    "sam_checkpoint": sam_vit_h_checkpoint,
-    "severities": [3],
-    "corruption_types": list(noise_dict.keys()),
-    "prompt_type": "bbox",
-    "metrics": ["dice", "iou"],
-    "primary_metric": "iou",
-    "device": "cuda",
-    "seed": 42,
-    "output_dir": OUTPUT_DIR,
-    "experiment_tag": "SAM_vit_h_coco"
-}
-coco_vit_h_experiment = Experiment(
-    model=sam_h_model, 
-    dataset=coco_dataset, 
-    noise_dict=noise_dict, 
-    severity=3
-)
-coco_vit_h_result = coco_vit_h_experiment._evaluate() 
-coco_vit_h_experiment._save_json(configurations=COCO_vit_h_configs)
-
-# 3 - MobileSAM 
-COCO_mobileSAM_configs = {
-    "dataset_name": "COCO",
-    "model_name": "MobileSAM_t",
-    "checkpoint": mobileSAM_checkpoint,
-    "severities": [3],
-    "corruption_types": list(noise_dict.keys()),
-    "prompt_type": "bbox",
-    "metrics": ["dice", "iou"],
-    "output_dir": OUTPUT_DIR,
-    "experiment_tag": "MobileSAM_t_coco"
-}
-coco_mobile_experiment = Experiment(
-    model=mobileSAM_model, 
-    dataset=coco_dataset, 
-    noise_dict=noise_dict, 
-    severity=3
-)
-coco_mobile_result = coco_mobile_experiment._evaluate() 
-coco_mobile_experiment._save_json(configurations=COCO_mobileSAM_configs)
-
-# 4 - FastSAM
-COCO_fastSAM_configs = {
-    "dataset_name": "COCO",
-    "model_name": "FastSAM_x",
-    "checkpoint": FastSAM_model, 
-    "severities": [3],
-    "corruption_types": list(noise_dict.keys()),
-    "prompt_type": "bbox",
-    "metrics": ["dice", "iou"],
-    "output_dir": OUTPUT_DIR,
-    "experiment_tag": "FastSAM_coco"
-}
-coco_fast_experiment = Experiment(
-    model=fastSAM_model, 
-    dataset=coco_dataset, 
-    noise_dict=noise_dict, 
-    severity=3
-)
-coco_fast_result = coco_fast_experiment._evaluate() 
-coco_fast_experiment._save_json(configurations=COCO_fastSAM_configs)
-
-
+for m_obj, m_name, m_ckpt in coco_models:
+    logging.info(f"Running COCO Evaluation for {m_name}, model_type: {m_name}, checkpoint: {m_ckpt}")
+    
+    coco_configs = {
+        "dataset_name": "COCO",
+        "model_name": m_name,
+        "checkpoint": m_ckpt,
+        "severities": severities,
+        "corruption_types": list(noise_dict.keys()),
+        "prompt_type": "bbox",
+        "metrics": ["dice", "iou", "hd95"],
+        "output_dir": OUTPUT_DIR,
+        "experiment_tag": f"{m_name}_coco"
+    }
+    
+    coco_experiment = Experiment(
+        model=m_obj, 
+        dataset=coco_dataset, 
+        noise_dict=noise_dict, 
+        severities=severities
+    )
+    coco_result, coco_failure_cases = coco_experiment._evaluate() 
+    coco_experiment._save_json(configurations=coco_configs)
 
 # ====== VOC EVALUATION =======
 voc_models = [
     (sam_b_model, "SAM_b", sam_vit_b_checkpoint),
     (sam_h_model, "SAM_h", sam_vit_h_checkpoint),
-    (mobileSAM_model, "MobileSAM_t", mobileSAM_checkpoint),
+    (mobileSAM_model, "MobileSAM", mobileSAM_checkpoint),
     (fastSAM_model, "FastSAM", FastSAM_model)
 ]
 
 for m_obj, m_name, m_ckpt in voc_models:
-    logging.info(f"Running VOC Evaluation for {m_name}...")
+    logging.info(f"Running VOC Evaluation for {m_name}, model_type: {m_name}, checkpoint: {m_ckpt} ")
     
     voc_configs = {
         "dataset_name": "VOC_Pascal",
         "model_name": m_name,
         "checkpoint": m_ckpt,
-        "severities": [3],
+        "severities": severities,
         "corruption_types": list(noise_dict.keys()),
         "prompt_type": "bbox",
-        "metrics": ["dice", "iou"],
+        "metrics": ["dice", "iou", "hd95"],
         "output_dir": OUTPUT_DIR,
         "experiment_tag": f"{m_name}_voc"
     }
@@ -222,9 +172,9 @@ for m_obj, m_name, m_ckpt in voc_models:
         model=m_obj, 
         dataset=voc_dataset, 
         noise_dict=noise_dict, 
-        severity=3
+        severities=severities
     )
-    voc_result = voc_experiment._evaluate() 
+    voc_result, voc_failure_cases = voc_experiment._evaluate() 
     voc_experiment._save_json(configurations=voc_configs)
 
 # ====== ADE20k EVALUATION =======  
@@ -237,16 +187,16 @@ ade_models = [
 ]
 
 for m_obj, m_name, m_ckpt in ade_models:
-    logging.info(f"Running ADE20k Evaluation for {m_name}...")
+    logging.info(f"Running ADE20k Evaluation for {m_name}, model_type: {m_name}, checkpoint: {m_ckpt}")
     
     ade_configs = {
         "dataset_name": "ADE20K",
         "model_name": m_name,
         "checkpoint": m_ckpt,
-        "severities": [3],
+        "severities": severities,
         "corruption_types": list(noise_dict.keys()),
         "prompt_type": "bbox",
-        "metrics": ["dice", "iou"],
+        "metrics": ["dice", "iou", "hd95"],
         "output_dir": OUTPUT_DIR,
         "experiment_tag": f"{m_name}_ade20k"
     }
@@ -255,7 +205,41 @@ for m_obj, m_name, m_ckpt in ade_models:
         model=m_obj, 
         dataset=ade_dataset, 
         noise_dict=noise_dict, 
-        severity=3
+        severities=severities
     )
-    ade_result = ade_experiment._evaluate() 
+    ade_result, ade_failure_cases = ade_experiment._evaluate() 
     ade_experiment._save_json(configurations=ade_configs)
+
+# ======  EVALUATION BSDS500======= 
+
+bsds_models = [
+    (sam_b_model, "SAM_b", sam_vit_b_checkpoint),
+    (sam_h_model, "SAM_h", sam_vit_h_checkpoint),
+    (mobileSAM_model, "MobileSAM_t", mobileSAM_checkpoint),
+    (fastSAM_model, "FastSAM", FastSAM_model)
+]
+
+for m_obj, m_name, m_ckpt in bsds_models: 
+    logging.info(f"Running BSDS500 Evaluation for {m_name}, model_type: {m_name}, checkpoint: {m_ckpt}") 
+
+    bsds_configs = { 
+        "dataset_name": "BSDS500",
+        "model_name": m_name,
+        "checkpoint": m_ckpt,
+        "severities": severities,
+        "corruption_types": list(noise_dict.keys()),
+        "prompt_type": "bbox",
+        "metrics": ["dice", "iou", "hd95"],
+        "output_dir": OUTPUT_DIR,
+        "experiment_tag": f"{m_name}_bsds500"
+    }
+
+    bsds_experiment = Experiment(
+        model=m_obj, 
+        dataset=bsds500_dataset, 
+        noise_dict=noise_dict, 
+        severities=severities 
+    )
+
+    bsds_result, bsds_failure_cases = bsds_experiment._evaluate() 
+    bsds_experiment._save_json(configurations=bsds_configs)
